105da1c8ab635bd4788dcabf27176b9d7f3bd768
ManuelCanoFig <-c>
2019-05-29 16:50:44 -0500

    Models, migrations up

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['title','content','category_id','featured','slug',
+    'model','brand'];
+
+    public function getFeaturedAttribute($featured){
+      return asset($featured);
+    }
+
+    protected $dates = ['delete_at'];
+
+    public function category(){
+      return $this->belongsTo('App\Category');
+    }
 }
